f12ab65189d2a47e40d3d53d3df38d283343e754
Rodrigo Pires <b.rodrigopires@gmail.com>
2021-02-19 11:04:20 -0300

    Obtendo um usuario por id

--- src/main/java/com/example/mongo/resources/UserResource.java
+++ src/main/java/com/example/mongo/resources/UserResource.java
@@ -2,11 +2,11 @@ package com.example.mongo.resources;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +35,12 @@ public class UserResource {
 			uDTOList.add(new UserDTO(u));
 		}
 		return uDTOList;
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(user));
 	}
 	
 }
---
+++ src/main/java/com/example/mongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.example.mongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
